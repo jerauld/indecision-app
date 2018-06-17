0780675a0dbd298ef482b5e31c3df50e03f14482
jerauld <25723428+jerauld@users.noreply.github.com>
2018-06-17 00:22:55 -0700

    played with jsx expressions and conditional rendering in jsx

--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
 console.log("App.js is running!");
 
-// JSX - JavaScript XML
-var template = <p>Does this change?</p>;
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+}
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? "Here are your options" : "No options" }</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'John Doe',
+    age: '26',
+    location: 'Lynchburg, Virginia'
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
