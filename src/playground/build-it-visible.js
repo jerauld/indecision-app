eefc7806037d379a7fd3864e1f10dd52e9a057d4
jerauld <25723428+jerauld@users.noreply.github.com>
2018-06-22 08:53:42 -0700

    updated built-it-visible and counter-example playground tests to include state

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,58 @@
-let visibility = false;
+class Visibility extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        }
+    }
 
-const toggleVisbility = () => {
-    visibility = !visibility;
-    render();
-};
+    handleToggleVisibility(){
+        this.setState((prevState) => {
+            console.log(prevState);
+            return {
+                visibility: !prevState.visibility
+            };
+        })
+    }
 
-const appRoot = document.getElementById('app');
+    render () {
+        return (
+            
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
+                {this.state.visibility && <p>Hey. These are some details you can now see!</p>}
+            </div>
+        )
+    }
+}
 
-const render = () => {
-  const jsx = (
-    <div>
-        <h1>Visibility Toggle</h1>
-        <button onClick={toggleVisbility}>
-            {visibility ? 'Hide details' : 'Show details'}
-        </button>
-        {visibility && <p>Hey. These are some details you can now see!</p>}
-        {/* You can next paragraph inside parenthesis and div */}
+ReactDOM.render(<Visibility />, document.getElementById('app'));
 
-    </div>
-  );
+// let visibility = false;
 
-  ReactDOM.render(jsx, appRoot);
-};
+// const toggleVisbility = () => {
+//     visibility = !visibility;
+//     render();
+// };
 
-render();
+// const appRoot = document.getElementById('app');
+
+// const render = () => {
+//   const jsx = (
+//     <div>
+//         <h1>Visibility Toggle</h1>
+//         <button onClick={toggleVisbility}>
+//             {visibility ? 'Hide details' : 'Show details'}
+//         </button>
+//         {visibility && <p>Hey. These are some details you can now see!</p>}
+//         {/* You can next paragraph inside parenthesis and div */}
+
+//     </div>
+//   );
+
+//   ReactDOM.render(jsx, appRoot);
+// };
+
+// render();
