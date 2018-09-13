64a8527c5d9dadf7a79030650459c5ab0de94821
jerauld <25723428+jerauld@users.noreply.github.com>
2018-09-12 21:14:37 -0700

    playing with exports

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
-// import { square, add } from './utils.js';
-import { isAdult, canDrink } from './person.js';
+// import './utilis.js';
+
+// import subtract, { square, add } from './utils.js';
 
 // console.log('app.js is running');
 // console.log(square(4));
 // console.log(add(100, 23));
+// console.log(subtract(100, 81));
 
+
+import isSenior, { isAdult, canDrink } from './person.js';
 console.log(isAdult(18));
 console.log(canDrink(18));
+console.log(isSenior(65));
+
+// Setting the default export and function
+// Grab the default and call it.
