e805d2ac8c871f247b4e5a23bf8754e1c710032c
jerauld <25723428+jerauld@users.noreply.github.com>
2018-09-17 15:24:56 -0700

    reconfigured webpack and babel for new @babel/

--- src/app.js
+++ src/app.js
@@ -1,18 +1,5 @@
-// import './utilis.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import subtract, { square, add } from './utils.js';
-
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
-
-
-import whyIsThisChickenNotisSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(whyIsThisChickenNotisSenior(65));
-
-// Setting the default export and function
-// Grab the default and call it.
+const template = <p>This is JSX from webpack</p>;
+ReactDOM.render(template, document.getElementById('app'));
